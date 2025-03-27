ab2a8ab085754a89f408fe7252701545f055585b
miki-babi <mikiyasshiferaw@gmail.com>
2025-03-27 11:42:12 +0300

    test2

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::post('/test',function(){
+    $test = new App\Models\Test();
+    $test->name = 'test';
+    $test->save();
+    return redirect()->route('result');
     dd('test');
     
 })->name('test');
+Route::get('/result',function(){
+    $test = App\Models\Test::all();
+    return $test;
+})->name('result');
